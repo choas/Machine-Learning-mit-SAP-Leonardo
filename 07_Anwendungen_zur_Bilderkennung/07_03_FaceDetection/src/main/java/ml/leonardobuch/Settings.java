a4229c84a1e51366fb28da1e07be18441b74d02a
Lars Gregori <lars.gregori@sap.com>
2019-05-14 01:34:31 +0200

    Setting.java ohne API KEY

---
+++ 07_Anwendungen_zur_Bilderkennung/07_03_FaceDetection/src/main/java/ml/leonardobuch/Settings.java
@@ -0,0 +1,6 @@
+package ml.leonardobuch;
+
+public final class Settings {
+  public static final String API_KEY = ; // API KEY einfuegen
+  public static final String PATH = "/MachineLearningMitSAPLeonardo/testdata/";
+}
---
+++ 10_Eigene_Maschine-Learning-Modelle_erstellen/10_02_ImageClassification_OAuth2/src/ml/leonardobuch/Settings.java
@@ -0,0 +1,10 @@
+package ml.leonardobuch;
+
+public final class Settings {
+  public static final String API_KEY = ; // API KEY einfuegen
+  public static final String PATH = "/MachineLearningMitSAPLeonardo/testdata/";
+
+  public static final String OAUTH2_URL = ; // url einfuegen
+  public static final String OAUTH2_CLIENT_ID = ; // clientid einfuegen
+  public static final String OAUTH2_CLIENT_SECRET = ; // clientsecret einfuegen
+  }
